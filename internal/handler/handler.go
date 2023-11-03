779b2ee69a8f9113504fffb5991b101791c238fa
ctuzelov <to.the.love.03@gmail.com>
2023-11-03 22:53:56 +0600

    method to check users for admin

--- internal/handler/handler.go
+++ internal/handler/handler.go
@@ -1,13 +1,35 @@
 package handler
 
-import "braincome/internal/service"
+import (
+	"braincome/internal/service"
+
+	"github.com/gin-gonic/gin"
+)
 
 type Handler struct {
-	service *service.Service
+	services *service.Service
 }
 
-func NewHandler(service *service.Service) *Handler {
-	return &Handler{
-		service: service,
+func NewHandler(services *service.Service) *Handler {
+	return &Handler{services: services}
+}
+
+func (h *Handler) InitRoutes() *gin.Engine {
+	router := gin.New()
+
+	auth := router.Group("/auth")
+	{
+		auth.POST("/sign-up", h.SignUp)
+		auth.POST("/sign-in", h.SignIn)
 	}
+
+	// Define a new group for admin-only routes
+	admin := router.Group("/admin")
+	admin.Use(h.Authenticate) // Apply the isAdmin middleware to the admin group
+
+	// Add the admin-only routes here
+	admin.GET("/users", h.GetUsers)
+	admin.GET("/user/:user_id", h.GetUser)
+
+	return router
 }
