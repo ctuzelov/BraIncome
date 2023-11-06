22e43df1888b8dd55661e777c954db35d1a8fd1f
ctuzelov <to.the.love.03@gmail.com>
2023-11-06 20:29:58 +0600

    clearly edited methods

--- internal/repository/manager.go
+++ internal/repository/manager.go
@@ -1,11 +1,22 @@
 package repository
 
+import (
+	"braincome/internal/models"
+
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
 type Authorization interface {
+	CreateUser(user models.User) (int, error)
+	GetUser(username, password string) (models.User, error)
 }
 
-type Service struct {
+type Repository struct {
+	Authorization
 }
 
-func NewRepository() *Service {
-	return &Service{}
+func NewRepository(db *mongo.Client) *Repository {
+	return &Repository{
+		Authorization: NewAuthMongo(db),
+	}
 }
