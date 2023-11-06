e2ff277fafff87dad4dc76a5cdfe2649fc1995e6
ctuzelov <to.the.love.03@gmail.com>
2023-11-06 20:34:31 +0600

    added methods into interface

--- internal/service/manager.go
+++ internal/service/manager.go
@@ -1,11 +1,25 @@
 package service
 
+import (
+	"braincome/internal/models"
+	"braincome/internal/repository"
+)
+
 type Authorization interface {
+	CreateUser(user models.User) (int, error)
+	GetUserByEmail(email string, password string) (models.User, error)
+}
+
+type User interface {
 }
 
 type Service struct {
+	Authorization
+	User
 }
 
-func NewService() *Service {
-	return &Service{}
+func NewService(repo *repository.Repository) *Service {
+	return &Service{
+		Authorization: NewAuthService(repo.Authorization),
+	}
 }
