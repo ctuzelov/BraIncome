c74cf40ab1212f6e61905d3838273208f1e98fc7
ctuzelov <to.the.love.03@gmail.com>
2023-11-06 20:31:01 +0600

    verifier added

---
+++ internal/helper/hasher.go
@@ -0,0 +1,29 @@
+package helper
+
+import (
+	"log"
+
+	"golang.org/x/crypto/bcrypt"
+)
+
+func HashPassword(password string) string {
+	bytes, err := bcrypt.GenerateFromPassword([]byte(password), 14)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return string(bytes)
+}
+
+func VerifyPassword(userPassword string, providedPassword string) (bool, string) {
+	err := bcrypt.CompareHashAndPassword([]byte(providedPassword), []byte(userPassword))
+	check := true
+	msg := ""
+
+	if err != nil {
+		msg = "email or password is incorrect"
+		check = false
+	}
+
+	return check, msg
+}
