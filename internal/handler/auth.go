a8b2f2d04e470e1efcbdca904d69efd4faccb387
ctuzelov <to.the.love.03@gmail.com>
2023-11-06 20:30:46 +0600

    fixed the way to connect to repo

--- internal/handler/auth.go
+++ internal/handler/auth.go
@@ -1,8 +1,51 @@
 package handler
 
-import "github.com/gin-gonic/gin"
+import (
+	"braincome/internal/models"
+	"braincome/internal/validator"
+	"log"
+	"net/http"
 
-func (h *Handler) SignUp(c *gin.Context) {}
+	"github.com/gin-gonic/gin"
+)
 
-func (h *Handler) SignIn(c *gin.Context) {}
+func (h *Handler) SignUp(c *gin.Context) {
+	var user models.User
 
+	if err := c.BindJSON(&user); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	validationErr := validator.GetErrMsgs(user)
+	if validationErr != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": validationErr})
+		return
+	}
+
+	resultInsertionNumber, err := h.services.CreateUser(user)
+	if err != nil {
+		log.Fatal(err)
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusOK, resultInsertionNumber)
+}
+
+func (h *Handler) SignIn(c *gin.Context) {
+	var user models.User
+
+	if err := c.BindJSON(&user); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	foundUser, err := h.services.GetUserByEmail(user.Email, user.Password)
+
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	c.JSON(http.StatusOK, foundUser)
+}
