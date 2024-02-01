255fb77f023d023146ce5bff8b6e1828bb8c51cd
ctuzelov <chingizkan.tuzelov@gmail.com>
2024-02-01 16:01:36 +0600

    changed the variable Expires to time.Time

--- internal/models/user.go
+++ internal/models/user.go
@@ -1,17 +1,19 @@
 package models
 
 import (
+	"time"
+
 	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
 type User struct {
-	ID               primitive.ObjectID `bson:"_id "`
-	First_name       string             `json:"first_name" validate:"required, min=2, max=100"`
-	Last_name        string             `json:"last_name" validate:"required, min=2, max=100"`
-	Password         string             `json:"password" validate:"required,min=6"`
-	Email            string             `json:"email" validate:"required,email"`
-	Token            *string            `json:"token"`
-	User_type        string             `json:"user_type" validate:"required,eq=ADMIN|eq=USER"`
-	Expires          *string            `json:"expires"`
-	AccessibleVideos []string           `json:"accessible_videos"`
+	ID         primitive.ObjectID `bson:"_id"`
+	First_name string             `json:"first_name" validate:"required, min=2, max=100"`
+	Last_name  string             `json:"last_name" validate:"required, min=2, max=100"`
+	Password   string             `json:"password" validate:"required,min=6"`
+	Email      string             `json:"email" validate:"required,email"`
+	Token      *string            `json:"token"`
+	User_type  string             `json:"user_type" validate:"required,eq=ADMIN|eq=USER"`
+	Expires    *time.Time         `bson:"expires" json:"expires"`
+	// AccessibleVideos []string           `json:"accessible_videos"`
 }
