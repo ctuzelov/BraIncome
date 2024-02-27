fc814851702ee837365a88fcc52ee216bd92dc60
ctuzelov <chingizkan.tuzelov@gmail.com>
2024-02-27 22:51:27 +0600

    course interface and instructore interface are declared

--- internal/repository/manager.go
+++ internal/repository/manager.go
@@ -18,12 +18,36 @@ type Authentication interface {
 	SwitchRole(token string) error
 }
 
+type Courses interface {
+	Insert(models.Course) error
+	Update(models.Course) error
+	GetAll() ([]models.Course, error)
+	GetById(id primitive.ObjectID) (models.Course, error)
+	GetByInstructor(instructor string) ([]models.Course, error)
+	GetByRating() ([]models.Course, error)
+	GetByEnrolled() ([]models.Course, error)
+	GetByCategory(category string) ([]models.Course, error)
+	GetByTitle(title string) ([]models.Course, error)
+	GetByLanguage(language string) ([]models.Course, error)
+}
+
+type Instructor interface {
+	Insert(models.Instructor) error
+	Update(models.Instructor) error
+	GetByName(name string) (models.Instructor, error)
+	GetByEmail(email string) (models.Instructor, error)
+}
+
 type Repository struct {
 	Authentication
+	Courses
+	Instructor
 }
 
 func NewRepository(db *mongo.Client) *Repository {
 	return &Repository{
 		Authentication: NewAuthMongo(db),
+		Courses:        NewCoursesMongo(db),
+		Instructor:     NewInstructorMongo(db),
 	}
 }
--- internal/service/manager.go
+++ internal/service/manager.go
@@ -3,6 +3,8 @@ package service
 import (
 	"braincome/internal/models"
 	"braincome/internal/repository"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
 type User interface {
@@ -13,12 +15,35 @@ type User interface {
 	MakeAdmin(email string) error
 }
 
+type Courses interface {
+	Insert(course models.Course) error
+	Update(course models.Course) error
+	GetAll() ([]models.Course, error)
+	GetById(id primitive.ObjectID) (models.Course, error)
+	GetByInstructor(instructor string) ([]models.Course, error)
+	GetByRating() ([]models.Course, error)
+	GetByEnrolled() ([]models.Course, error)
+	GetByCategory(category string) ([]models.Course, error)
+	GetByTitle(title string) ([]models.Course, error)
+}
+
+type Instructor interface {
+	Insert(course models.Instructor) error
+	Update(course models.Instructor) error
+	GetByName(name string) (models.Instructor, error)
+	GetByEmail(email string) (models.Instructor, error)
+}
+
 type Service struct {
 	User
+	Courses
+	Instructor
 }
 
 func NewService(repo *repository.Repository) *Service {
 	return &Service{
-		User: NewAuthService(repo.Authentication),
+		User:       NewAuthService(repo.Authentication),
+		Courses:    NewCourseService(repo.Courses),
+		Instructor: NewInstructorService(repo.Instructor),
 	}
 }
